582f57864c7c6ecab3f4892e649016155033c772
Nikengiri Maheshgiri Goswami <66886066+nikengoswami@users.noreply.github.com>
2021-09-22 14:48:44 +0530

    Updated

--- 01_array/01_reverse_array.cpp
+++ 01_array/01_reverse_array.cpp
@@ -6,31 +6,44 @@
 
 
 // ----------------------------------------------------------------------------------------------------------------------- //
-// TC: O(N)
-void rvereseArray(int arr[], int start, int end)
-{
-    while (start < end)
-    {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-    }
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
 }
 
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  //Your code here
+  string res="";
+  int slen=str.length()-1;
+  int i=0;
+  int j = 0;
+  for(int i=slen;i>=0;i--){
+      res += str[i];
+  }
+  return res;
+}
 
 // ----------------------------------------------------------------------------------------------------------------------- //
-// TC: O(N)
-void rvereseArray(int arr[], int start, int end)
-{
-    if (start >= end)
-        return;
-
-    int temp = arr[start];
-    arr[start] = arr[end];
-    arr[end] = temp;
-
-    // Recursive Function calling
-    rvereseArray(arr, start + 1, end - 1);
-}

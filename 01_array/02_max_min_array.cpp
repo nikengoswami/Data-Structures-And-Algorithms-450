378c2c8f5a6ebdcbe4b28f0013c7f74c8cd010c4
Nikengiri Maheshgiri Goswami <66886066+nikengoswami@users.noreply.github.com>
2021-09-22 15:55:15 +0530

    Update 02_max_min_array.cpp

--- 01_array/02_max_min_array.cpp
+++ 01_array/02_max_min_array.cpp
@@ -1,7 +1,6 @@
 /*
     link: https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
-
-    can also be done using several methods, visit link for more methods
+    
 */
 
 
@@ -11,12 +10,12 @@ int main()
 {
     int arr[] = { 1000, 11, 445,
                 1, 330, 3000 };
-    int arr_size = 6;
+    int size = 6;
 
     int mx = arr[0];
     int mn = arr[1];
 
-    for (int i = 1;i < arr_size;i++) {
+    for (int i = 1;i < size;i++) {
         if (arr[i] > mx) mx = arr[i];
         if (arr[i] < mn) mn = arr[i];
     }

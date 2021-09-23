5b38612a5bb1e7ece4df3b2f86b4c324a7cf28ae
Nikengiri Maheshgiri Goswami <66886066+nikengoswami@users.noreply.github.com>
2021-09-23 14:39:38 +0530

    Update 06_union_and_intersection.cpp

--- 01_array/06_union_and_intersection.cpp
+++ 01_array/06_union_and_intersection.cpp
@@ -1,34 +1,47 @@
 /*
     link: https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1
 
-    sol: https://www.geeksforgeeks.org/find-union-and-intersection-of-two-unsorted-arrays/
-    for different approach
+    
 */
 
 
 // ----------------------------------------------------------------------------------------------------------------------- //
-int doUnion(int a[], int n, int b[], int m) {
-    set<int> s;
-    for (int i = 0;i < n;i++) s.insert(a[i]);
-    for (int i = 0;i < m;i++) s.insert(b[i]);
-    return s.size();
-}
 
-int doIntersection(int a[], int n, int b[], int m) {
-    sort(a, a + n);
-    sort(b, b + m);
-
-    int i = 0, j = 0, ans = 0;
-
-    while (i <= j) {
-        if (a[i] == b[j]) {
-            ans++;
-            i++;
-            j++;
+//                                      FIRST APPROACH                              //
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        int res[n+m]={0};
+        int d=0;
+        
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i]==b[j]){
+                d++;
+            }
         }
-        else if (a[i] < b[j]) i++;
-        else j++;
     }
-    return ans;
-}
+      return n+m-d;  
+    }
+};
 
+//                     OPTIMIZED ALGO  //
+
+class Solution{
+    public:
+    int doUnion(int a[], int n, int b[], int m)  {
+          int gtr = n<m?m:n;
+        set<int> s;
+       for(int i = 0;i<big;i++) {
+           if (i<n) {
+           s.insert(a[i]);
+           }
+           if (i<m) {
+           s.insert(b[i]);
+     }
+ }
+       return s.size();
+   }
+};

a23230353211134cd4418198bfcaff44ead3eff9
Nikengiri Maheshgiri Goswami <66886066+nikengoswami@users.noreply.github.com>
2021-10-06 23:01:20 +0530

    Update 11_find_duplicates.cpp

--- 01_array/11_find_duplicates.cpp
+++ 01_array/11_find_duplicates.cpp
@@ -6,32 +6,23 @@
 
 // ----------------------------------------------------------------------------------------------------------------------- //
 /*
-    perfect solution. (very tricky)
-    here we take every element and make negative to its 0 based index hence whenever we get again positive int. its the ans.
-
-    for eg. nums = [3,1,3,4,2]
-    here as 3 is repeating observe that while tracing whole program (3-1=2 to make 0 based) position will be negative and will again become positive.
-*/
-int findDuplicate(vector<int>& nums) {
-    int size = nums.size();
-    for (int i = 0;i < size;i++) {
-        int n = abs(nums[i]) - 1;
-        nums[n] = -nums[n];
-        if (nums[n] > 0) return n + 1;
-    }
-    return 0; // of no use just to avoid error
-}
-
-
-// ----------------------------------------------------------------------------------------------------------------------- //
-// space comp.: less than n. but not O(1)
-int findDuplicate(vector<int>& nums) {
-    set<int> s;
-    for (int i = 0;i < nums.size();i++) {
-        if (s.find(nums[i]) != s.end()) {
-            return nums[i];
+ class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int k=0;
+        for(int i =0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums.at(i)==nums.at(j)){
+                   k=nums.at(i);
+                    break;
+                }
+            }
         }
-        s.insert(nums[i]);
+     return k;
     }
-    return 1;
-}
+    
+};
+
+// --------------------------------------------------TLE--------------------------------------------------------------------- //
+
+

ade579fe0edcbaa162c32d362f0509287b5441ce
Nikengiri Maheshgiri Goswami <66886066+nikengoswami@users.noreply.github.com>
2022-05-30 16:42:34 +0530

    optimized - 3 solutions-{2 for loops,1 for loop & dp}

--- 01_array/13_kadane's_algo.cpp
+++ 01_array/13_kadane's_algo.cpp
@@ -4,6 +4,57 @@
 
 
 // ----------------------------------------------------------------------------------------------------------------------- //
+//----------TIME LIMIT EXCEEDED----------//
+
+long maxSum = -999999999;
+       for (int i = 0; i < n; i++) {
+           int sum = 0;
+           for (int j = i; j < n; j++) {
+               sum = sum + arr[j];
+               if (sum > maxSum) {
+                   maxSum = sum;
+               }
+           }
+       }
+       return maxSum;
+
+
+
+//----------------------------------OPTIMIZED------------------------------------------------------------------------------//
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+       
+      // Your code here
+       int  max = INT_MIN;
+       int  mth=0;
+       
+       for(int  i=0;i<n;i++){
+           mth+=arr[i];
+           if(mth>max){
+               max =mth;
+           }
+           
+           if(mth<0){
+               mth =0;
+           }
+       }
+       
+       return max ;
+}
+
+};
+
+
+// ----------------------------------------------------------USING DP------------------------------------------------------------- //
+
+
+
+
 int maxSubarraySum(int arr[], int n) {
     int dp[n];
     dp[0] = max(INT_MIN, arr[0]);
